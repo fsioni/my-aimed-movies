bfac64be9e4d83cf3cd26945e9b2d1e1ccb59e80
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-02-27 16:50:49 +0100

    Resize logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Logo() {
     return (
-        <Image priority src="/logo.png" alt="Logo My Aimed Movies" width={500} height={500}/>
+        <Image priority src="/logo.png" alt="Logo My Aimed Movies" width={200} height={200}/>
     )
 }
