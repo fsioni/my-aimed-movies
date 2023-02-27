0c8860ad2f71b1547437c9513cfb04a6ef9d28a6
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-02-27 16:52:16 +0100

    MenuItem type to properly map on the array + added style to navbar

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,21 +1,29 @@
+"use client";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 export default function Navbar() {
+    interface menuItem {
+        title: string,
+        urlTo: string,
+        isActive: boolean
+    }
+
+    const menu: Array<menuItem> = [
+        {title: "Home", urlTo: "/", isActive: usePathname() === "/"},
+        {title: "Movies", urlTo: "/movies", isActive: usePathname() === "/movies"},
+        {title: "Series", urlTo: "/series", isActive: usePathname() === "/series"},
+        {title: "Actors", urlTo: "/actors", isActive: usePathname() === "/actors"},
+    ]
+
     return (
         <nav>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <Link href="/movies">Movies</Link>
-                </li>
-                <li>
-                    <a href="/series">Series</a>
-                </li>
-                <li>
-                    <a href="/actors">Actors</a>
-                </li>
+            <ul className="flex flex-row gap-4">
+                {menu.map(({title, urlTo, isActive}) => (
+                    <li key={title}>
+                        <Link href={urlTo}
+                              className={`${isActive ? "text-gray-700 font-bold underline" : "text-gray-600 font-semibold hover:underline"}`}>{title}</Link>
+                    </li>))}
             </ul>
         </nav>
     )
