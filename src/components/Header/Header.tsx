34a39f44a62d5cf719f34e3b22a9333225b8076b
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-02-27 16:51:08 +0100

    Added style to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/Header/Navbar";
 
 export default function Header() {
     return (
-        <header>
+        <header className="flex items-center border-2">
             <Logo/>
             <Navbar/>
         </header>
