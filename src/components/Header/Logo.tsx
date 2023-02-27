9dc7223f7b56e6b52e4bf2dbe3ea5eb8b4d552aa
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-02-27 23:34:00 +0100

    Changed logo
    Ca a pris 10 minutes

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Logo() {
     return (
-        <Image priority src="/logo.png" alt="Logo My Aimed Movies" width={200} height={200}
-               className="mr-3 h-6 sm:h-9"/>
+        <Image priority src="/logo.png" alt="Logo My Aimed Movies" width={500} height={268}
+               className="mr-3 h-16 w-auto"/>
     )
 }
