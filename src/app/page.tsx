37eb79df3a6197efc7aa10783e3099277c44e46a
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-03-03 14:22:56 +0100

    (Home):Removed dark: properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(): JSX.Element {
     return (
         <>
-            <h1 className="text-4xl pt-3 text-amber-700 dark:h-screen dark:bg-gray-900">Home</h1>
+            <h1 className="text-4xl pt-3 text-amber-700">Home</h1>
         </>
     )
 }
