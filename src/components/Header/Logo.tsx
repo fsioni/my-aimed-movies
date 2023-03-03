f30d9f433852653c2f0c859ec924ebcde6d8ad5e
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-03-03 14:22:29 +0100

    (Logo):Light background on darkmode

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,6 +3,6 @@ import Image from "next/image";
 export default function Logo() {
     return (
         <Image priority src="/logo.png" alt="Logo My Aimed Movies" width={500} height={268}
-               className="mr-3 h-16 w-auto"/>
+               className="mr-3 h-16 w-auto dark:bg-gray-400 dark:rounded"/>
     )
 }
